e1b48562e7c2888a1d66706fffdab348870fbe26
galloramiro <ramiro.r.gallo@gmail.com>
2020-08-01 15:05:47 -0300

    add interactive ploter and interactive service function to the factorie

--- work/analizers/services/factories.py
+++ work/analizers/services/factories.py
@@ -1,12 +1,18 @@
 from services.movies.files_handlers import MoviesCsvHandler
-from services.movies.ploter import StaticPlots
+from services.movies.ploter import StaticPlots, InteractivePlot
 from services.movies.service import MoviesService
 
 
 class ServicesFactorie:
     
-    def get_movies_static_analyse_service(self) -> MoviesService:
+    def get_static_movies_analyse_service(self) -> MoviesService:
         return MoviesService(
             files_handler=MoviesCsvHandler(),
             ploter=StaticPlots()
+        )
+    
+    def get_interactive_movies_analyse_service(self) -> MoviesService:
+        return MoviesService(
+            files_handler=MoviesCsvHandler(),
+            ploter=InteractivePlot()
         )
--- work/analizers/services/movies/ploter.py
+++ work/analizers/services/movies/ploter.py
@@ -1,3 +1,4 @@
+import altair as alt
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 import seaborn as sns
@@ -62,4 +63,57 @@ class StaticPlots:
             height=self._HEIGHT,
             aspect=self._ASPECT,
         )
+        return plot
+
+    
+class InteractivePlot:
+    def plot_reveneu_vs_budget(self, data: DataFrame) -> FacetGrid:
+        plot = self._make_lmplot_by_year(
+            x=Columns.budget.value, y=Columns.revenue.value, data=data,
+            title="Movies: Reveneu vs Budget by Year"
+        )
+        return plot
+
+    def plot_popularity_vs_budget(self, data: DataFrame) -> FacetGrid:
+        plot = self._make_lmplot_by_year(
+            x=Columns.budget.value, y=Columns.popularity.value, data=data,
+            title="Movies: Popularity vs Budget by Year"
+        )
+        return plot
+    
+    def plot_reveneu_vs_votes_average(self, data: DataFrame) -> FacetGrid:
+        plot = self._make_lmplot_by_year(
+            x=Columns.vote_average.value, y=Columns.revenue.value, data=data,
+            title="Movies: Reveneu vs Votes Average by Year"
+        )   
+        return plot
+    
+    def plot_popularity_vs_votes_average(self, data: DataFrame) -> FacetGrid:
+        plot = self._make_lmplot_by_year(
+            x=Columns.vote_average.value, y=Columns.popularity.value, data=data,
+            title="Movies: Popularity vs Votes Average by Year"
+        )
+        return plot
+    
+    def plot_budget_vs_votes_average(self, data: DataFrame) -> FacetGrid:
+        plot = self._make_lmplot_by_year(
+            x=Columns.vote_average.value, y=Columns.budget.value, data=data,
+            title="Movies: Budget vs Votes Average by Year"
+        )   
+        return plot
+    
+    def _make_lmplot_by_year(self, x: str, y: str, data: DataFrame, title:str) -> FacetGrid:
+        click = alt.selection_multi(encodings=['color'])
+        x_line = alt.X(f"{x}:Q")
+        y_line = alt.Y(f"{y}:Q")
+        color = alt.condition(
+            click, alt.Color(
+                shorthand=f"{Columns.release_year.value}:N"
+            ), alt.value('lightgray')
+        )
+        
+        plot = alt.Chart(data).mark_circle(
+        ).encode(x=x_line, y=y_line,  color=color
+                ).properties(width=600, title=title).add_selection(click)
+
         return plot
